9c9aa96d8334507b3526435a6dfe0d6a5510ad33
MellyMel Meinyk <105060907+Vimala2022@users.noreply.github.com>
2022-05-13 12:00:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,16 +51,21 @@ except URLError as e:
 streamlit.stop()
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("insert into fruit_load_list values ('from streamlit')") #Fix the control of Flow
-
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchall() #replace fetchone with fetchall
-
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+#Snowflake-releated functions
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+     my_cur.execute("SELECT * from fruit_load_list")
+   return my_cur.fetchall() 
+
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list() #replace fetchone with fetchall with function as the last resort
+   streamlit.dataframe(my_data_rows)
+   
+   my_cur.execute("insert into fruit_load_list values ('from streamlit')") #Fix the control of Flow
+
 
 #Allow the user to add a fruit to the list
 add_my_fruit =  streamlit.text_input('What fruit would you like to add?')
